14252b3936f1e8dfc8eeca11c955f0e88a20972a
hexeva <patrick79.g@gmail.com>
2022-02-16 15:24:57 +0100

    created migration,model and seeder

---
+++ database/migrations/2022_02_16_134422_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50);
+            $table->text('description');
+            $table->string('thumb',150);
+            $table->decimal('price',5,2);
+            $table->string('series',30);
+            $table->date('sale_date');
+            $table->string('type',20);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
