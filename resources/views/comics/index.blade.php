f2f16c29ba0b6c82a9f3f48d9d54d3d82144f9a5
hexeva <patrick79.g@gmail.com>
2022-02-16 17:08:58 +0100

    created header and index page

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+
+@section('main_content')
+    <div class="container">
+        <div class="comic_list">
+            <div class="row row-cols-lg-5 g-2 g-lg-3 ">
+                @foreach ($comics as $comic)
+
+                {{-- single card --}}
+                <div class="card mx-3" style="width: 18rem;">
+                    <img src="{{$comic->thumb}}" class="card-img-top" alt="">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$comic->title}}</h5>
+                      <p class="card-text">{{$comic->description}}</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                {{-- end card --}}
+                    
+                @endforeach
+                
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,21 @@
 <body>
 
     <header>
-        Io sono l'header
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                  <a class="nav-link active mx-4" aria-current="page" href="#">Home</a>
+                  <a class="nav-link mx-4" href="#">Features</a>
+                  <a class="nav-link mx-4" href="#">Pricing</a>
+                  <a class="nav-link disabled mx-4">Disabled</a>
+                </div>
+              </div>
+            </div>
+          </nav>
     </header>
 
     <main>
